4f98189bd9e73226ca0a04d5ad942cc3855ded00
John Tracy <johntracy@Johns-MacBook-Pro.local>
2016-04-23 10:27:03 -0400

    asset folders added

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// array of objects which holds each character, names, their hit points, attack points and counter attack points.
+// starwars characters Jar Jar Binks, Darth vader, Han Solo, Luke Skywalker.
+
+var characterArr = [{
+	name: 'Jar Jar Binks',
+	health: ,
+	attack: ,
+	counterAttack: ,
+}, {
+	name: 'Darth Vader',
+	health: ,
+	attack: ,
+	counterAttack: ,
+}, {
+	name: 'Han Solo',
+	health: ,
+	attack: ,
+	counterAttack: ,
+}, {
+	name: 'Luke Skywalker',
+	health: ,
+	attack: ,
+	counterAttack: ,
+}];
